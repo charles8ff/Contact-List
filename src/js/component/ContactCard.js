67751359142d561348f9b52ffe78c210ef3d9462
Carlos Fisac Ferrández <charles8ff@gmail.com>
2021-02-13 19:15:24 +0000

    fixed photo

--- src/js/component/ContactCard.js
+++ src/js/component/ContactCard.js
@@ -8,7 +8,7 @@ export const ContactCard = props => {
 		<li className="list-group-item">
 			<div className="row w-100">
 				<div className="col-12 col-sm-6 col-md-3 px-0">
-					<img src={Photo} alt="Mike Anamendolla" className="rounded-circle mx-auto d-block img-fluid" />
+					<img src={Photo} alt="haha no load" className="rounded-circle mx-auto d-block img-fluid" />
 				</div>
 				<div className="col-12 col-sm-6 col-md-9 text-center text-sm-left">
 					<div className=" float-right">
